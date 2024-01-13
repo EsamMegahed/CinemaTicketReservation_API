43e597123bc1f42a823cd00bceaf58327718e295
EsamMegahed <esammegahed34366@gmail.com>
2024-01-13 05:18:54 +0200

    add 2 more wayes to create Json response

--- tickets/urls.py
+++ tickets/urls.py
@@ -3,7 +3,11 @@ from. import views
 
 
 urlpatterns = [
-
-    path('django/jsonResbonseNoModel/',views.no_rest_no_model()),
+    # [1] - Without Rest and no model query FBV
+    path('django/jsonResbonseNoModel/',views.no_rest_no_model),
+    # [2] - model data default django without rest
+    path('django/jsonResbonseFromModel/',views.no_rest_from_model),
+    # [3] - Function Based View
+    path('django/fbv-list/',views.FBV_List),
    
 ]
--- tickets/views.py
+++ tickets/views.py
@@ -1,13 +1,19 @@
 from http import HTTPStatus
 from django.shortcuts import render
 from django.http.response import JsonResponse
+from.models import Guest,Movie,Reservation
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+from .serializers import GuestSerializer,MoiveSerializer,ReservationSerializer
 # Create your views here.
 
 # All Methods To Create View API
 
-# [1] - Without Rest and no model query FBV
+
 def no_rest_no_model(request):
-    guests = {
+    guests = [
         {
             'id':1,
             'Name':'Ahmed',
@@ -18,5 +24,33 @@ def no_rest_no_model(request):
             'Name':'Esam',
             'mobile':'5456123',
         },
+    ]
+    response = JsonResponse(guests,safe=False)
+    return response
+
+
+# [2] - model data default django without rest
+def no_rest_from_model(request):
+    data = Guest.objects.all()
+    respons ={
+        'guests':list(data.values('name','phone_number'))
     }
-    return JsonResponse(guests,status=HTTPStatus.INTERNAL_SERVER_ERROR,safe=False)
+    return JsonResponse(respons)
+
+
+# [3] - Function Based View
+#3.1 Get POST
+@api_view(['GET','POST'])
+def FBV_List(request):
+    #GET
+    if request.method == 'GET':
+        guests = Guest.objects.all()
+        serializer = GuestSerializer(guests,many= True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = GuestSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.data,status=status.HTTP_400_BAD_REQUEST)
+#3.1 Get PUT DELETE
